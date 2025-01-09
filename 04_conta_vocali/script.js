e0e392a874ecb1bd0888ad1c0631b68a78e5a5db
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-01-09 21:20:34 +0100

    04_conta_vocali final

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,22 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    for (let char of str.toLowerCase()) {
+        
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    
+    return count;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const vowels = countVowels("javascript");
+console.log(vowels);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
