2a1f60264d3b82bea599954504c5e075f11911f3
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-01-09 17:39:02 +0100

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,15 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Abba';
+
+
+// Dichiara la funzione qui.
+function saluto(userName) {
+    return("Ciao" + " " + userName + "!")
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(saluto(userName));
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
