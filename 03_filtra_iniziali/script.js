c270fbda0564b611cf98213f8f3700ebc703da97
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-01-09 20:45:41 +0100

    03-filtra-iniziali-final

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filterWordsByLetter(wordsArray, letter) {
+    const result = [];
+    let targetLetter = "a";
+    for (let i = 0; i < wordsArray.length; i++) {
+        const word = wordsArray[i];
+        if (word[0].toLowerCase() === targetLetter) {
+            result.push(word);
+          }
+        }
+      
+        return result;
+      }
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const filteredNames = filterWordsByLetter(names, "A");
+console.log(filteredNames);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
